931248a7a739e9de000e4a36bce3765371bcc45a
Max Kottong <max.kottong@gmail.com>
2022-08-17 12:24:33 -0400

    .

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,5 @@
 INSERT INTO candidates (first_name, last_name, industry_connected)
 VALUES
-  ('Ronald', 'Firbank', 1),
   ('Virginia', 'Woolf', 1),
   ('Piers', 'Gaveston', 0),
   ('Charles', 'LeRoi', 1),
